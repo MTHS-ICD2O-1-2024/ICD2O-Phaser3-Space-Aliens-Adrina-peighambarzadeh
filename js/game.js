9bdf32de35f64767a3857bb447d3139f2bf34ef9
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-04-30 17:18:52 +0000

    init

--- js/game.js
+++ js/game.js
@@ -1,49 +1,52 @@
 /* global Phaser */
-
-// Copyright (c) 2025 Shanea Jaromay All rights reserved
+// Copyright (c) 2025 Ain Jeong All rights reserved
 //
 // Created by: Adrina peighambarzadeh
 // Created on: Apr 2025
 // This is the Phaser3 game configuration file
 
 // scene import statements
-
-import SplashScene from './splashScene.js'
-import TitleScene from './titleScene.js'
+import SplashScene from "./splashScene.js"
+import TitleScene from "./titleScene.js"
+import MenuScene from "./menuScene.js"
 
 //* Game scene */
 const splashScene = new SplashScene()
 const titleScene = new TitleScene()
-
+const menuScene = new MenuScene()
 /**
- * Start Phaser Game.
- */
+* Start Phaser Game.
+*/
+
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
-      debug: true
-    }
+      debug: true,
+    },
   },
+
   // set background color
   backgroundColor: 0xffffff,
+
   scale: {
     mode: Phaser.Scale.FIT,
     // we place it in the middle of the page.
-    autoCenter: Phaser.Scale.CENTER_BOTH
-  }
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
+
 const game = new Phaser.Game(config)
 // console.log(game)
-
 // load scenes
 // Note: remember any "key" is global and CANNOT be reused
-
-game.scene.add('splashScene', splashScene)
-game.scene.add('titleScene', titleScene)
+game.scene.add("splashScene", splashScene)
+game.scene.add("titleScene", titleScene)
+game.scene.add("menuScene", menuScene)
 
 // the start scene
-game.scene.start('splashScene')
+game.scene.start("splashScene")
---
+++ js/menuScene.js
@@ -0,0 +1,59 @@
+/* global Phaser */
+// Copyright (c) 2025 Ain Jeong All rights reserved
+//
+// Created by: Adrina peighambarzadeh
+// Created on: Apr 2025
+// This is the Phaser3 game configuration file
+/**
+* This class is the Menu Scene.
+*/
+
+class MenuScene extends Phaser.Scene {
+  /**
+ * This method is the constructor.
+ */
+  constructor() {
+    super({ key: "menuScene" })
+  }
+  /**
+ * Can be defined on your own Scenes.
+ * This method is called by the Scene Manager when the scene starts,
+ *  before preload() and create().
+ * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+ */
+  init(data) {
+    this.cameras.main.setBackgroundColor("ffffff")
+  }
+  /**
+ * Can be defined on your own Scenes.
+ * Use it to load assets.
+ */
+
+  preload() {
+    console.log("Menu Scene")
+  }
+
+  /**
+
+ * Can be defined on your own Scenes.
+ * Use it to create your game objects.
+ * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+ */
+
+  create(data) {
+    // pass
+  }
+
+  /**
+ * Should be overridden by your own Scenes.
+ * This method is called once per game step while the scene is running.
+ *  @param {number} time - The current time.
+ *  @param {number} delta - The delta time in ms since the last frame.
+ */
+
+  update(time, delta) {
+    // pass
+  }
+}
+
+export default MenuScene
