d6fb91291a43b24fc53afcd7216b9e78c2b4e606
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-01 00:39:30 +0000

    init

--- js/game.js
+++ js/game.js
@@ -1,7 +1,8 @@
 /* global Phaser */
-// Copyright (c) 2025 Ain Jeong All rights reserved
+
+// Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Adrina peighambarzadeh
+// Created by: Adrina. peighambarzadeh
 // Created on: Apr 2025
 // This is the Phaser3 game configuration file
 
@@ -9,44 +10,45 @@
 import SplashScene from "./splashScene.js"
 import TitleScene from "./titleScene.js"
 import MenuScene from "./menuScene.js"
+import GameScene from "./gameScene.js"
 
-//* Game scene */
+// create the new scenes
 const splashScene = new SplashScene()
 const titleScene = new TitleScene()
 const menuScene = new MenuScene()
-/**
-* Start Phaser Game.
-*/
+const gameScene = new GameScene()
 
+/**
+ * Start Phaser Game.
+ */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-
   physics: {
     default: "arcade",
     arcade: {
       debug: true,
     },
   },
-
   // set background color
-  backgroundColor: 0xffffff,
-
+  backgroundColor: 0x5f6e7a,
   scale: {
     mode: Phaser.Scale.FIT,
-    // we place it in the middle of the page.
+    // we place it in the middle of the page
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
 }
 
 const game = new Phaser.Game(config)
 // console.log(game)
+
 // load scenes
-// Note: remember any "key" is global and CANNOT be reused
+// Note: remember any "key" is global and CAN NOT be reused!
 game.scene.add("splashScene", splashScene)
 game.scene.add("titleScene", titleScene)
 game.scene.add("menuScene", menuScene)
+game.scene.add("gameScene", gameScene)
 
 // the start scene
 game.scene.start("splashScene")
--- js/gameScene.js
+++ js/gameScene.js
@@ -3,21 +3,18 @@
 // Copyright (c) 2025 Adrina. peighambarzadeh All rights reserved
 //
 // Created by: Adrina. peighambarzadeh
-// Created on: Apl 2025
-// This is the Menu Scene
+// Created on: Apr 2025
+// This is the Game Scene
 
 /**
- * This class is the Menu Scene.
+ * This class is the Game Scene.
  */
-class MenuScene extends Phaser.Scene {
+class GameScene extends Phaser.Scene {
   /**
    * This method is the constructor.
    */
   constructor() {
-    super({ key: "menuScene" })
-
-    this.menuSceneBackgroundImage = null
-    this.startButton = null
+    super({ key: "gameScene" })
   }
 
   /**
@@ -35,9 +32,7 @@ class MenuScene extends Phaser.Scene {
    * Use it to load assets.
    */
   preload() {
-    console.log("Menu Scene")
-    this.load.image("menuSceneBackground", "./assets/aliens_screen_image2.jpg")
-    this.load.image("startButton", "./assets/start.png")
+    console.log("Game Scene")
   }
 
   /**
@@ -46,25 +41,18 @@ class MenuScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-    this.menuSceneBackgroundImage = this.add.sprite(0, 0, "menuSceneBackground")
-    this.menuSceneBackgroundImage.x = 1920 / 2
-    this.menuSceneBackgroundImage.y = 1080 / 2
-
-    this.startButton = this.add.sprite(1920 / 2, 1080 / 2 + 100, "startButton")
-    this.startButton.setInteractive({ useHandCursor: true })
-    this.startButton.on("pointerdown", () => this.clickButton())
+    // pass
   }
 
   /**
    * Should be overridden by your own Scenes.
+   * This method is called once per game step while the scene is running.
    * @param {number} time - The current time.
    * @param {number} delta - The delta time in ms since the last frame.
    */
-  update(time, delta) {}
-
-  clickButton() {
-    this.scene.start("gameScene")
+  update(time, delta) {
+    // pass
   }
 }
 
-export default MenuScene
+export default GameScene
