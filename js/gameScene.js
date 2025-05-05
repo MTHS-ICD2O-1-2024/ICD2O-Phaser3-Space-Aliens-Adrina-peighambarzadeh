918554f341590457e7cab380b8b01d0f6a1c8cf1
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-05 23:21:35 +0000

    init

--- js/gameScene.js
+++ js/gameScene.js
@@ -1,11 +1,9 @@
 /* global Phaser */
-
-// Copyright (c) 2025 Adrina peighambarzadeh All rights reserved
+// Copyright (c) 2025 Ain Jeong All rights reserved
 //
-// Created by: Adrina. peighambarzadeh
+// Created by: Adrina peighambarzadeh
 // Created on: Apr 2025
-// This is the Game Scene
-
+// This is the Phaser3 game configuration file
 /**
  * This class is the Game Scene.
  */
@@ -15,44 +13,45 @@ class GameScene extends Phaser.Scene {
    */
   constructor() {
     super({ key: "gameScene" })
+    this.background = null
+    this.ship = null
   }
-
   /**
    * Can be defined on your own Scenes.
    * This method is called by the Scene Manager when the scene starts,
-   * before preload() and create().
+   *  before preload() and create().
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   init(data) {
-    this.cameras.main.setBackgroundColor("#ffffff")
+    this.cameras.main.setBackgroundColor("ffffff")
   }
-
   /**
    * Can be defined on your own Scenes.
    * Use it to load assets.
    */
   preload() {
     console.log("Game Scene")
+    this.load.image("starBackground", "./assets/starBackground.png")
+    this.load.image("ship", "./assets/spaceShip.png")
   }
-
   /**
    * Can be defined on your own Scenes.
    * Use it to create your game objects.
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-    // pass
+    this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
+    this.background.setOrigin(0, 0)
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
   }
-
   /**
    * Should be overridden by your own Scenes.
    * This method is called once per game step while the scene is running.
-   * @param {number} time - The current time.
-   * @param {number} delta - The delta time in ms since the last frame.
+   *  @param {number} time - The current time.
+   *  @param {number} delta - The delta time in ms since the last frame.
    */
   update(time, delta) {
     // pass
   }
 }
-
 export default GameScene
