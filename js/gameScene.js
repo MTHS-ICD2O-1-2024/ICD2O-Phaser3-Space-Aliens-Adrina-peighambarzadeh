69310a960af41a62847636df9e9aa4dd2bee6b1d
Adrina-peighambarzadeh <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-08 17:59:20 -0400

    Update gameScene.js

--- js/gameScene.js
+++ js/gameScene.js
@@ -9,87 +9,66 @@
  * This class is the Game Scene.
  */
 class GameScene extends Phaser.Scene {
-  /**
-   * This method is the constructor.
-   */
   constructor() {
     super({ key: "gameScene" })
+    
     this.background = null
     this.ship = null
-    this.missileGroup = null
     this.fireMissile = false
   }
 
-  /**
-   * Called when the scene starts.
-   * @param {object} data - Optional data passed to the scene.
-   */
   init(data) {
     this.cameras.main.setBackgroundColor("ffffff")
   }
 
-  /**
-   * Load game assets here.
-   */
   preload() {
     console.log("Game Scene")
-    this.load.image("starBackground", "./assets/starBackground.png")
-    this.load.image("ship", "./assets/spaceShip.png")
-    this.load.image("missile", "./assets/missile.png") // load missile
-  }
 
-  /**
-   * Create game objects here.
-   * @param {object} data - Optional data passed to the scene.
-   */
+    // image
+    this.load.image('starBackground', './assets/starBackground.png')
+    this.load.image('ship', './assets/spaceShip.png')
+    this.load.image('missile', './assets/missile.png') 
+  }
+  
   create(data) {
-    // Add background
-    this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
+    this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
     this.background.setOrigin(0, 0)
 
-    // Add ship
-    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
 
     // Create missile group
     this.missileGroup = this.physics.add.group()
-
-    // Input keys
-    this.keyLeft = this.input.keyboard.addKey("LEFT")
-    this.keyRight = this.input.keyboard.addKey("RIGHT")
-    this.keySpace = this.input.keyboard.addKey("SPACE")
   }
+ update (time, delta) {
+    //clled 60 times a second, hopefully!
+   
+    const keyLeft0bj = this.input.keyboard.addKey('LEFT')
+    const keyRight0bj = this.input.keyboard.addKey('RIGHT')
+    const keySpace0bj = this.input.keyboard.addKey('SPACE')
 
-  /**
-   * Update logic runs 60 times per second.
-   * @param {number} time - Current time.
-   * @param {number} delta - Time since last frame.
-   */
-  update(time, delta) {
-    // Move left
-    if (this.keyLeft.isDown) {
+    if (keyLeft0bj.isDown === true) {
       this.ship.x -= 15
       if (this.ship.x < 0) {
         this.ship.x = 0
       }
     }
 
-    // Move right
-    if (this.keyRight.isDown) {
+    if (keyRight0bjt.isDown === true) {
       this.ship.x += 15
       if (this.ship.x > 1920) {
         this.ship.x = 1920
       }
     }
-
-    // Fire missile
-    if (this.keySpace.isDown && this.fireMissile === false) {
+if (keySpace0bj.isDown === true) {
+    if (this.fireMissile === false) {
+      //fire missile
       this.fireMissile = true
-      const missile = this.physics.add.sprite(this.ship.x, this.ship.y, "missile")
-      this.missileGroup.add(missile)
+      const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
+      this.missileGroup.add(aNewMissile)
+      }
     }
-
-    // Reset fire lock when SPACE is released
-    if (this.keySpace.isUp) {
+   
+    if (keySpace0bj.isUp === true) {
       this.fireMissile = false
     }
   }
