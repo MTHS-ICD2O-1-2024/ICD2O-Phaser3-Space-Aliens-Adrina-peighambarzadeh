280c468aa1e39d70ea4197b651eb6990af523bf6
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-20 17:17:31 +0000

    chnaged game scne

--- js/gameScene.js
+++ js/gameScene.js
@@ -1,128 +1,118 @@
 /* global Phaser */
-
-// Copyright (c) 2025 Shanea Jaromay All rights reserved
+// Copyright (c) 2025 Adrina peighambarzadeh All rights reserved
 //
-// Created by: Shanea Jaromay
-// Created on: May 2025
-// This is the Game Scene
-
+// Created by: Adrina peighambarzadeh 
+// Created on: Apr 2025
+// This is the Phaser3 game configuration file
 /**
- * This class is the Game Scene.
- */
+* This class is the Game Scene.
+*/
 class GameScene extends Phaser.Scene {
-  // create an alien
+  // Create an alien
   createAlien () {
-    const alienXLocation = Math.floor(Math.random() * 1920) + 1 // this will get a number between 1 and 1920 
-    let alienXVelocity = Math.floor(Math.random() * 50) + 1 // this will get a number between 1 and 50;
-    alienXVelocity *= Math.round(Math.random()) ? 1 : -1 // this will add minus sign in 50% of cases
+    const alienXLocation = Math.floor(Math.random() * 1920) + 1
+    let alienXVelocity = Math.floor(Math.random() * 50) + 1
+    alienXVelocity *= Math.round(Math.random()) ? 1 : -1
     const anAlien = this.physics.add.sprite(alienXLocation, -100, 'alien')
     anAlien.body.velocity.y = 200
     anAlien.body.velocity.x = alienXVelocity
     this.alienGroup.add(anAlien)
   }
-
-  constructor() {
+  /**
+  * This method is the constructor.
+  */
+  constructor () {
     super({ key: 'gameScene' })
-
     this.background = null
     this.ship = null
     this.fireMissile = false
   }
-
   /**
    * Can be defined on your own Scenes.
    * This method is called by the Scene Manager when the scene starts,
    *  before preload() and create().
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
-  init(data) {
-    this.cameras.main.setBackgroundColor('#ffffff')
+  init (data) {
+    this.cameras.main.setBackgroundColor('ffffff')
   }
-
   /**
    * Can be defined on your own Scenes.
    * Use it to load assets.
    */
-  preload() {
+  preload () {
     console.log('Game Scene')
-
-    // image
-    this.load.image('startBackground', './assets/starBackground.png')
+    // images
+    this.load.image('starBackground', './assets/starBackground.png')
     this.load.image('ship', './assets/spaceShip.png')
     this.load.image('missile', './assets/missile.png')
     this.load.image('alien', './assets/alien.png')
     // sound
     this.load.audio('laser', './assets/laser1.wav')
+    this.load.audio('explosion', './assets/barrelExploding.wav')
   }
-
   /**
    * Can be defined on your own Scenes.
    * Use it to create your game objects.
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
-  create(data) {
-    this.background = this.add.image(0, 0, 'startBackground').setScale(2.0)
+  create (data) {
+    this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
     this.background.setOrigin(0, 0)
-
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
-
     // create a group for the missiles
     this.missileGroup = this.physics.add.group()
-
-  // create a group for the aliens
+    // create a group for the aliens
     this.alienGroup = this.add.group()
     this.createAlien()
-  }
-
-
+    // Collisions between missiles and aliens
+    this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
+      alienCollide.destroy()
+      missileCollide.destroy()
+      this.sound.play('explosion')
+      this.createAlien()
+      this.createAlien()
+  }.bind(this))
+ }
   /**
    * Should be overridden by your own Scenes.
    * This method is called once per game step while the scene is running.
    *  @param {number} time - The current time.
    *  @param {number} delta - The delta time in ms since the last frame.
    */
-  update(time, delta) {
+  update (time, delta) {
     // called 60 times a second, hopefully!
-
     const keyLeftObj = this.input.keyboard.addKey('LEFT')
     const keyRightObj = this.input.keyboard.addKey('RIGHT')
     const keySpaceObj = this.input.keyboard.addKey('SPACE')
-
     if (keyLeftObj.isDown === true) {
       this.ship.x -= 15
       if (this.ship.x < 0) {
         this.ship.x = 0
       }
     }
-
     if (keyRightObj.isDown === true) {
       this.ship.x += 15
       if (this.ship.x > 1920) {
         this.ship.x = 1920
       }
     }
-
     if (keySpaceObj.isDown === true) {
       if (this.fireMissile === false) {
         // fire missile
         this.fireMissile = true
-        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
+        const aNewMissile = this.physics.add.sprite(
+          this.ship.x,
+          this.ship.y,
+          'missile'
+        )
         this.missileGroup.add(aNewMissile)
         this.sound.play('laser')
       }
     }
-
     if (keySpaceObj.isUp === true) {
       this.fireMissile = false
     }
-
-    this.missileGroup.children.each(function (item) {
-      item.y = item.y - 15
-      if (item.y < 0) {
-        item.destroy()
-      }
-    })
-  }
-}
-
-export default GameScene
+    this.missileGroup.children.each(function
+ 
+ 
